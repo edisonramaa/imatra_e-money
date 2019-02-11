4006b4263962b5de52a7af596d362eca139434d4
akumal <anil.kumal@seepiagames.com>
2019-02-12 01:16:57 +0200

    initial project setup

---
+++ e-money-web/src/main/java/com/emoney/EmoneyWebStarter.java
@@ -0,0 +1,24 @@
+package com.emoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+/**
+ * Created by Anil Kumal on 12/2/2019.
+ */
+@SpringBootApplication(
+        exclude = {DataSourceAutoConfiguration.class},
+        scanBasePackages = {"com.emoney"}
+)
+public class EmoneyWebStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(EmoneyWebStarter.class, args);
+    }
+
+
+}
+
+
+
