8886ed377d68822c2a69b2353cd5f137e155fcaf
Edison Rama <edison.r@scopicsoftware.com>
2019-03-13 06:13:18 +0200

    benefits implementation progress

--- e-money-admin/src/main/java/com/emoney/admin/controller/BenefitController.java
+++ e-money-admin/src/main/java/com/emoney/admin/controller/BenefitController.java
@@ -7,10 +7,19 @@ import com.emoney.admin.model.BenefitEntity;
 import com.emoney.admin.service.IBenefitService;
 import com.emoney.core.constant.WebResourceConstant;
 import com.emoney.core.controller.ControllerBase;
+import com.emoney.core.model.ResponseObj;
 import com.emoney.core.utils.impl.BeanMapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by Edison Rama on 03/05/2019.
@@ -21,7 +30,7 @@ public class BenefitController extends ControllerBase {
     public static final String BASE_URL = WebResourceConstant.EMONEY.BENEFIT;
 
     @Autowired
-    public BenefitController(IBenefitService userService) {
-        super(userService, new BeanMapperImpl(BenefitEntity.class, BenefitRequestDto.class), new BeanMapperImpl(BenefitEntity.class, BenefitResponseDto.class));
+    public BenefitController(IBenefitService benefitService) {
+        super(benefitService, new BeanMapperImpl(BenefitEntity.class, BenefitRequestDto.class), new BeanMapperImpl(BenefitEntity.class, BenefitResponseDto.class));
     }
 }
--- e-money-admin/src/main/java/com/emoney/admin/dto/responseDto/BenefitResponseDto.java
+++ e-money-admin/src/main/java/com/emoney/admin/dto/responseDto/BenefitResponseDto.java
@@ -4,9 +4,17 @@ import com.emoney.core.model.ResponseDtoBase;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
+
 
 @Getter
 @Setter
 public class BenefitResponseDto extends ResponseDtoBase {
-
+    private String name;
+    private String description;
+    private String streetAddress;
+    private Date startDateTime;
+    private Date endDateTime;
+    private int credits;
+    private String qrCode;
 }
