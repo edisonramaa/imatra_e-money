e483046a6ba29b9d5acae399de2e00af368f0879
Enxhi <enxhi.minaj@leaddesk.com>
2019-03-27 09:42:19 +0200

    services ui

---
+++ e-money-ui/src/main/ng-client/src/app/all-view/services/service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './service.component.html',
+  styleUrls: ['./service.component.scss']
+})
+export class ServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
