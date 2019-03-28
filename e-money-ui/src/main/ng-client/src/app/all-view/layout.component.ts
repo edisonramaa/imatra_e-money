95636997b20be4776e9d0825d8cdd1d1a7da8330
erakuraja <kurajaera@gmail.com>
2019-03-28 07:03:52 +0200

    user profile updates

--- e-money-ui/src/main/ng-client/src/app/all-view/layout.component.ts
+++ e-money-ui/src/main/ng-client/src/app/all-view/layout.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {ICREDIT_URL, PROFILE_URL} from "../core/utility/navigation-url";
+import {Router} from "@angular/router";
+import {MatSidenav} from "@angular/material";
 
 
 @Component({
@@ -7,8 +10,20 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+    @ViewChild("sidenav")
+    sideNav: MatSidenav;
 
+    constructor(private _router: Router) {
+    }
 
     ngOnInit(): void {
+    }
+
+    openUserProfile() {
+        let finalUrl = "/" + ICREDIT_URL + "/" + PROFILE_URL;
+        this.sideNav.toggle();
+        this.sideNav.autoFocus = false;
+        this._router.navigateByUrl(finalUrl);
+
     }
 }
