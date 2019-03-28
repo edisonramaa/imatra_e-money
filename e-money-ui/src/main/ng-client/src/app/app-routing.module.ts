0284f084df22d76c40bc7825b0ad4fa93f14f6e9
Anil Kumal <anil.kumal@student.lut.fi>
2019-03-28 06:26:43 +0200

    change default startup page to login page

--- e-money-ui/src/main/ng-client/src/app/app-routing.module.ts
+++ e-money-ui/src/main/ng-client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'icredit', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'icredit', loadChildren: './all-view/layout.module#LayoutModule'},
     {path: 'login', loadChildren: './login/login.module#LoginModule'}
   //{path: '**', redirectTo: 'login'}
