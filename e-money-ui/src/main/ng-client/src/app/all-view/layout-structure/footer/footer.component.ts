8763ced71072ac340cdb69ef8c4312fdc8a34036
erakuraja <kurajaera@gmail.com>
2019-03-28 03:14:16 +0200

    footer navigation update

--- e-money-ui/src/main/ng-client/src/app/all-view/layout-structure/footer/footer.component.ts
+++ e-money-ui/src/main/ng-client/src/app/all-view/layout-structure/footer/footer.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {
   FIND_JOB_URL,
   ICREDIT_URL,
-  MAIN_URL,
-  MY_JOB_URL, PAY_URL,
+  MY_JOB_URL,
+  PAY_URL,
   REDEEM_URL,
   WALLET_URL
 } from "../../../core/utility/navigation-url";
@@ -22,22 +22,47 @@ export class FooterComponent implements OnInit {
   }
 
 openFindJob() {
+  if (document.querySelectorAll(".footer-button-container.selected").length > 0) {
+    document.querySelector(".footer-button-container.selected").classList.remove("selected");
+  }
+  document.querySelectorAll(".footer-button-container")[0].classList.add("selected");
+
     let finalUrl = "/"+ICREDIT_URL+  "/" + FIND_JOB_URL;
     this._router.navigateByUrl(finalUrl);
 }
   openMyjob(){
+    if (document.querySelectorAll(".footer-button-container.selected").length > 0) {
+      document.querySelector(".footer-button-container.selected").classList.remove("selected");
+    }
+    document.querySelectorAll(".footer-button-container")[1].classList.add("selected");
+
     let finalUrl = "/"+ICREDIT_URL+ "/" + MY_JOB_URL;
     this._router.navigateByUrl(finalUrl);
   }
   openRedeem(){
+    if (document.querySelectorAll(".footer-button-container.selected").length > 0) {
+      document.querySelector(".footer-button-container.selected").classList.remove("selected");
+    }
+    document.querySelectorAll(".footer-button-container")[2].classList.add("selected");
+
     let finalUrl = "/"+ICREDIT_URL+  "/" + REDEEM_URL;
     this._router.navigateByUrl(finalUrl);
   }
   openWallet() {
+    if (document.querySelectorAll(".footer-button-container.selected").length > 0) {
+      document.querySelector(".footer-button-container.selected").classList.remove("selected");
+    }
+    document.querySelectorAll(".footer-button-container")[3].classList.add("selected");
+
     let finalUrl = "/"+ICREDIT_URL+  "/" + WALLET_URL;
     this._router.navigateByUrl(finalUrl);
   }
   openPayment(){
+    if (document.querySelectorAll(".footer-button-container.selected").length > 0) {
+      document.querySelector(".footer-button-container.selected").classList.remove("selected");
+    }
+    document.querySelectorAll(".footer-button-container")[4].classList.add("selected");
+
     let finalUrl = "/"+ICREDIT_URL+  "/" + PAY_URL;
     this._router.navigateByUrl(finalUrl);
   }
